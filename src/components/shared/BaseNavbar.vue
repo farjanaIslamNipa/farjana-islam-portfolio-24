3aff24c8c51ec2b7b64bf1d92a8fd98d697fcde0
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-09-07 23:06:40 +0600

    make nvbar responsive

--- src/components/shared/BaseNavbar.vue
+++ src/components/shared/BaseNavbar.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full fixed top-0">
-    <div class="app-container bg-dark-blue/10 dark:bg-white/10 backdrop-blur-sm backdrop-filter rounded-2xl mt-6 py-6 text-gray-200">
-      <div class="flex justify-center items-center gap-x-8">
-        <RouterLink to="/projects" class="nav-item">Projects</RouterLink>
-        <RouterLink to="/projects" class="nav-item">About</RouterLink>
-        <RouterLink to="/projects" class="nav-item">Contact</RouterLink>
-        <button class="px-4 py-1 rounded-lg  text-orange-600 font-medium hover:bg-apple-green">Resume</button>
+    <div class="app-container">
+      <div class="bg-dark-blue/10 dark:bg-white/10 backdrop-blur-sm backdrop-filter rounded-2xl mt-6 py-4 sm:py-6 text-gray-200">
+        <div class="flex justify-center items-center  sm:gap-x-5 md:gap-x-8">
+          <RouterLink to="/projects" class="nav-item">Projects</RouterLink>
+          <RouterLink to="/projects" class="nav-item">About</RouterLink>
+          <RouterLink to="/projects" class="nav-item">Contact</RouterLink>
+          <button class="px-3 sm:px-4 py-1 rounded-lg  text-orange-600 font-medium hover:bg-apple-green">Resume</button>
+        </div>
       </div>
     </div>
 <button @click="toggleDarkMode" class="bg-slate-400 px-3 py-1 rounded-md">
