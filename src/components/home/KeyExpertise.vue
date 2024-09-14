7a452d3eb04d2d7d83bc20d3c07aa8719994b4da
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-09-14 23:28:57 +0600

    working on key expertise section

---
+++ src/components/home/KeyExpertise.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white/5 mt-14">
+    <div class="app-container py-8">
+      <h1 class="text-3xl font-semibold">Key Expertise</h1>
+      <div class="grid grid-cols-3 gap-x-5 py-10">
+        <div class="bg-app-black p-4 rounded-xl">Web Design</div>
+        <div class="bg-mint-green p-4 rounded-xl">Web Development</div>
+        <div class="bg-app-black p-4 rounded-xl">Graphic Design</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
