64794b8ac974bc1b075bbd64f72c1a4c185f5984
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-09-15 13:07:13 +0600

    Set layout of key expertise section

--- src/components/home/KeyExpertise.vue
+++ src/components/home/KeyExpertise.vue
@@ -3,7 +3,27 @@
     <div class="app-container py-8">
       <h1 class="text-3xl font-semibold">Key Expertise</h1>
       <div class="grid grid-cols-3 gap-x-5 py-10">
-        <div class="bg-app-black p-4 rounded-xl">Web Design</div>
+        <div class="bg-app-black p-4 rounded-xl">
+          <!-- <div class="max-w-md mx-auto shadow-md rounded-lg overflow-hidden">
+            <div class="p-4">
+                <img src="/images/web-design.svg" alt="Rounded Image" class="rounded-xl float-left mr-4 mb-4 h-[100px]">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros non ligula varius tristique. Phasellus et orci nisi. Integer malesuada, arcu eu vestibulum pellentesque, sapien lorem dapibus mauris, vel vehicula leo elit in enim. Pellentesque ut turpis in mi vestibulum condimentum. Nullam feugiat ex id risus consequat, et tincidunt libero tincidunt.
+                </p>
+            </div>
+          </div> -->
+          <div class="flex w-full gap-x-4">
+            <div class="">
+              <div class="bg-[#181028] rounded-full p-3 -ml-8 -mt-8 h-[120px] w-[120px] flex justify-center items-center">
+                <img src="/images/web-design.svg" alt="Web Design" class="rounded-xl h-[100px]">
+              </div>
+            </div>
+            <div>
+              <p class="text-xl font-semibold text-apple-green">Web Design</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur</p>
+            </div>
+          </div>
+        </div>
         <div class="bg-mint-green p-4 rounded-xl">Web Development</div>
         <div class="bg-app-black p-4 rounded-xl">Graphic Design</div>
       </div>
